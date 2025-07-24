66df793f56434c560193b1a62870024dbf1545e3
goscha01 <sayapingeorge@gmail.com>
2025-07-24 13:07:55 -0400

    changed env name

--- zenbooker/src/components/customer-modal.jsx
+++ zenbooker/src/components/customer-modal.jsx
@@ -19,7 +19,7 @@ const CustomerModal = ({ isOpen, onClose, onSave }) => {
   const [showAddressSuggestions, setShowAddressSuggestions] = useState(false)
 
   // Google Places API key
-  const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+  const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
 
   useEffect(() => {
     if (!isOpen) {
